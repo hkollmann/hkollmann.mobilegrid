fff6a4a3aeddded34049a89c1183c87e541283a6
Henner Kollmann <henner.kollmann@gmx.de>
2019-01-15 19:29:30 +0100

    use auto install

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,13 +3,7 @@ set -e
 set -x
 
 npm install qxcompiler -g 
-#git clone --depth=10 https://github.com/qooxdoo/qooxdoo-compiler.git
-#cd qooxdoo-compiler
-#npm link
-#cd ..
-
 qx contrib update 
 qx contrib list
 qx contrib install qooxdoo/qxl.apiviewer 
-qx contrib install qooxdoo/qxl.versionlabel
 qx compile --target=build -v
